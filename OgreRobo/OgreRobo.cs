56c0a95bd6b617f407780fc90599ed0f3ba81ef6
mbellier <mbellier>
2013-10-16 11:13:43 +0200

    Initial commit, including project files, media files and minimal requirement for MogreSDK to run binaries.

---
+++ OgreRobo/OgreRobo.cs
@@ -0,0 +1,27 @@
+﻿
+using Mogre;
+using Mogre.TutorialFramework; // provides a simple 'BaseApplication' class
+using System;
+
+namespace OgreRobo
+{
+    class OgreRobo : BaseApplication
+    {
+        public static void Main()
+        {
+            new OgreRobo().Go();
+        }
+
+        protected override void CreateScene()
+        {
+            mSceneMgr.AmbientLight = new ColourValue(1, 1, 1);
+
+            Entity ent = mSceneMgr.CreateEntity("Head", "ogrehead.mesh");
+
+            SceneNode node = mSceneMgr.RootSceneNode.CreateChildSceneNode("HeadNode");
+
+            node.AttachObject(ent);
+        }
+
+    }
+}
